cb9b1836df21c018b6a15c7454b8e076e55bd26f
Mateusz Dariusz Szymczyk <35966164+BeMoreHappY@users.noreply.github.com>
2023-12-10 17:25:45 +0100

    Dodane zdjecie

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import photo from "@/app/assets/foto.jpg";
 
 export default function Home() {
    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         Wieprz
+      <main className="flex min-h-screen flex-col items-center p-24">
+         <h1 className="text-center text-6xl font-black">
+            Kladia <span className="text-blue-500">Hlubek</span>
+         </h1>
+         <img src={photo.src} alt="Kladuia" />
       </main>
    );
 }
