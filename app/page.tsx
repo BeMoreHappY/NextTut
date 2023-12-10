701f74deb3c437c7b0fa7b06b858e05e07c96248
Mateusz Dariusz Szymczyk <35966164+BeMoreHappY@users.noreply.github.com>
2023-12-10 17:35:59 +0100

    Dodany przycisk

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+"use client";
 import Image from "next/image";
 import photo from "@/app/assets/foto.jpg";
+import { useState } from "react";
 
 export default function Home() {
+   const [isClicked, setIsClicked] = useState(false);
    return (
       <main className="flex min-h-screen flex-col items-center p-24">
          <h1 className="text-center text-6xl font-black">
-            Kladia <span className="text-blue-500">Hlubek</span>
+            Chcesz zobaczyć dobrą dupę?
          </h1>
-         <img src={photo.src} alt="Kladuia" />
+         <button
+            onClick={() => setIsClicked(!isClicked)}
+            className="mb-10 rounded bg-blue-500 px-4 py-2 text-7xl font-bold text-white hover:bg-blue-700"
+         >
+            Kliknij tutaj
+         </button>
+         {isClicked && <img src={photo.src} alt="Kladuia" />}
       </main>
    );
 }
